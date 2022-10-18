9b65914220b499e6dc1c8c4a9bb202f53d2c3688
Dennis Wang <denniswang2488@gmail.com>
2022-10-18 08:26:22 -0400

    update

---
+++ src/C and C++/Rename.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    printf("hello, world\n");
+    std::cout << "Hello World" << std::endl;
+
+    short num0 = 10;
+    int num1 = 20;
+    long num2 = 30;
+    long long num3 = 40;
+    int one = 1;
+
+    cout<<"占用空间为"<<sizeof(num0)<<endl;
+}
